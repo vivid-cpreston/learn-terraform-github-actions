57f67278f799740bf50fee0190d8ecb282f18c8c
VIVIDSOLUTIONS\cpreston <cpreston@vividsolutions.com>
2022-01-19 17:17:30 -0800

    no message

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "Vivid Solutions"
+    organization = "vivid-solutions"
 
     workspaces {
       name = "gh-actions-test"
